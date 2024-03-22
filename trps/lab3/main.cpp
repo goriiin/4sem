57dee170421448d550fd8eb52f77a54796b6b842
goriiin <afk00.17@bk.ru>
2024-03-23 02:19:29 +0300

    lab3

--- trps/lab3/main.cpp
+++ trps/lab3/main.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
+#include <string>
 
+struct rec{
+    std::string FIO;
+    short Year;
+    std::string Adr;
+    std::string Tel;
+};
 
 int main(){
+    FILE* Ftp;
+    long Mark;
+    rec V;
+    std::string s, Fn;
+    char c;
 
+    std::cin >> Fn;
+    Ftp = fopen(Fn.c_str(), "r");
+
+    if (Ftp){
+        Mark = 0;
+        while (fread(&V, sizeof(V),1, Ftp)!=EOF){
+            std::cout << V.FIO <<std::endl;
+            std::cout << V.Year << std::endl;
+            std::cout << V.Adr << std::endl;
+            std::cout << V.Tel << std::endl;
+        }
+    }
 }
