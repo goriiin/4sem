259274822d6b35e81d04500843694dcad65a8975
dmitry <dkoshenkofff@mail.ru>
2024-04-26 11:51:46 +0300

    lab5 - fix

--- ASM/labs/lab5/src/change_order_asm.asm
+++ ASM/labs/lab5/src/change_order_asm.asm
@@ -1,93 +1,92 @@
-section .data
-    wordNumber db 1 ; Номер слова для обработки
-
-section .bss
-    inputString resb 256
-    outputString resb 256
-    stack resb 256 ; Стек для временного хранения символов слов
-
-; Обязательно назвать также как назвали в С++
 section .text
-    global change_order
-    extern print_result ; подключаем с++ функцию для вывода
+global change_order
+extern print_result
 
 change_order:
-    ; Вывод приглашения к вводу
-    mov rax, 1
-    mov rdi, 1
-    mov rsi, prompt1
-    mov rdx, 18
-    syscall
+    ; save registers
+    push rbp
+    mov rbp, rsp
 
-    ; Чтение строки
-    mov rdi, 0
-    mov rsi, inputString
-    mov rdx, 256
-    syscall
+    ; get arguments
+    mov rsi, rdi    ; input string
+    mov rdx, rsi    ; size of input string
+    mov rcx, rdx    ; num of word to change
 
-    ; Обработка строки
-    mov rsi, inputString
-    mov rdi, stack
-    mov rcx, 256
-    mov r8, 1 ; Счетчик слов
-    process:
-        lodsb
-        cmp al, ' '
-        jz pushToStack
-        cmp al, 0
-        jz processStack
-        stosb
-        jmp process
+    ; count number of words in input string
+    xor rax, rax
+    mov r8, rsi
+.count_words:
+    xor rax, rax    ; word counter
+    mov r8, rsi    ; input string
+    mov al, byte [r8]
+    test al, al
+    jz .count_words_done
+    cmp al, ' '
+    je .skip_space
+    inc rax        ; increment word counter
+.loop:
+    mov al, byte [r8]
+    test al, al
+    jz .count_words_done
+    cmp al, ' '
+    je .skip_space
+    inc r8
+    jmp .loop
+.skip_space:
+    inc r8
+    jmp .loop
+.count_words_done:
 
-    pushToStack:
-        mov al, [rsi]
-        cmp al, ' '
-        jz skipPush
-        push rax
-        skipPush:
-        inc rsi
-        loop process
+.find_word:
+    mov r8, rsi    ; input string
+    xor rax, rax    ; word counter
+    mov r9, rcx    ; n-th word to find
+.find_word_loop:
+    cmp rax, r9
+    je .found_word
+    mov al, byte [r8]
+    test al, al
+    jz .error
+    cmp al, ' '
+    je .next_word
+    inc r8
+    jmp .find_word_loop
+.next_word:
+    inc rax        ; increment word counter
+    inc r8
+    jmp .find_word_loop
+.found_word:
+    ; r8 now points to the n-th word
 
-    processStack:
-        mov rcx, 256
-        mov rdi, outputString
-        popLoop:
-            pop rax
-            stosb
-        loop popLoop
+    ; change order of letters in the word
+    mov r9, r8
+    xor rax, rax
+.change_order:
+    mov al, byte [r9]
+    test al, al
+    jz .done_change_order
+    cmp al, ' '
+    je .done_change_order
+    xchg al, byte [r9 + rax]
+    inc rax
+    jmp .change_order
+.done_change_order:
 
-    ; Проверка номера слова
-    cmp r8, wordNumber
-    jne error
+    ; print result
+    mov rdi, r8
+    call print_result
 
-    ; Вывод обработанного слова
-    mov rax, 1
-    mov rdi, 1
-    mov rsi, prompt2
-    mov rdx, 19
-    syscall
-    mov rdi, outputString
-    mov rcx, 256
-    print:
-        lodsb
-        cmp al, 0
-        je end
-        mov rax, 1
-        mov rdi, 1
-        mov rdx, 1
-        syscall
-        jmp print
+    ; restore registers
+    mov rsp, rbp
+    pop rbp
+    ret
 
-    error:
-        ; Вывод ошибки
-        mov rax, 1
-        mov rdi, 1
-        mov rsi, errorMsg
-        mov rdx, 5
-        syscall
+.error:
+    mov rdi, error_message
+    call print_result
+    mov rsp, rbp
+    pop rbp
+    ret
 
-    end:
-        ; Завершение программы
-        mov rax, 60
-        xor rdi, rdi
-        syscall
+section .data
+error_message db 'ERROR', 0
